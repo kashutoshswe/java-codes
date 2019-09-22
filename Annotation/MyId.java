8e080673cae77ec79e88344163ed2b4ccb8e392e
Ashutosh Kumar <ashutoshkumardbms@gmail.com>
2019-09-22 23:33:55 +0530

    MyId Annotation

---
+++ Annotation/MyId.java
@@ -0,0 +1,14 @@
+package com.cdac;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MyId {
+}
